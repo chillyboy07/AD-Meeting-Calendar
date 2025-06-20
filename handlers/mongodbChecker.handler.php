1de5c78a0cd086fb6011efffa5f98d15a6da0687
chillyboy07 <bonneviechilly@gmail.com>
2025-06-20 15:22:39 +0800

    refactor: changed code for mongobdb

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,11 +1,13 @@
 <?php
+
+require_once __DIR__ . '/../utils/envSetter.util.php';
+
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:27111");
+    $mongoUri = "mongodb://{$typeConfig['mongo']['user']}:{$typeConfig['mongo']['pass']}@{$typeConfig['mongo']['host']}:{$typeConfig['mongo']['port']}/?authSource=admin";
+    $manager = new MongoDB\Driver\Manager($mongoUri);
 
-    $command = new MongoDB\Driver\Command(["ping" => 1]);
-    $mongo->executeCommand("admin", $command);
-
-    echo "✅ Connected to MongoDB successfully.  <br>";
+    $manager->executeCommand($typeConfig['mongo']['db'], new MongoDB\Driver\Command(['ping' => 1]));
+    echo "✅ Connected to MongoDB successfully. <br>";
 } catch (MongoDB\Driver\Exception\Exception $e) {
-    echo "❌ MongoDB connection failed: " . $e->getMessage() . "  <br>";
+    echo "❌ MongoDB connection failed: " . $e->getMessage();
 }
