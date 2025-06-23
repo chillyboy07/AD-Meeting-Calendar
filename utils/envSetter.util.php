2607f1f2b644ee629088848493f15feaf66df02e
chillyboy07 <bonneviechilly@gmail.com>
2025-06-23 11:49:15 +0800

    refactor: changed code in env

--- utils/envSetter.util.php
+++ utils/envSetter.util.php
@@ -1,30 +1,22 @@
 <?php
 
-define('BASE_PATH', realpath(__DIR__ . '/../') . '/');
-define('UTILS_PATH', BASE_PATH . 'utils');
-
-require_once BASE_PATH . 'vendor/autoload.php';
+if (!defined('BASE_PATH')) {
+    define('BASE_PATH', realpath(dirname(__DIR__)));
+}
+require_once BASE_PATH . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
-// Override PG host to localhost if on Windows
-$pgHost = PHP_OS_FAMILY === 'Windows' ? 'localhost' : $_ENV['PG_HOST'];
+$mongoConfig = [
+    'uri' => $_ENV['MONGO_URI'],
+    'db' => $_ENV['MONGO_DB']
+];
 
-$typeConfig = [
-    'postgres' => [
-        'host' => $pgHost,
-        'port' => $_ENV['PG_PORT'],
-        'user' => $_ENV['PG_USER'],
-        'pass' => $_ENV['PG_PASS'],
-        'db'   => $_ENV['PG_DB'],
-    ],
-    'mongo' => [
-        'host' => $_ENV['MONGO_HOST'],
-        'port' => $_ENV['MONGO_PORT'],
-        'user' => $_ENV['MONGO_USER'],
-        'pass' => $_ENV['MONGO_PASS'],
-        'db'   => $_ENV['MONGO_DB'],
-    ],
-    'key' => $_ENV['ENV_NAME'],
+$pgConfig = [
+    'host' => $_ENV['PG_HOST'],
+    'port' => $_ENV['PG_PORT'],
+    'db' => $_ENV['PG_DB'],
+    'user' => $_ENV['PG_USER'],
+    'pass' => $_ENV['PG_PASS']
 ];
